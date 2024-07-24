bc5fc292e52cbb41fcab43d7eb2caaa6d7018ab1
Dmytro Reva <dmytroreva2001@gmail.com>
2024-07-24 14:00:54 +0200

    Agregado formulario reactivo con validaciones

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../services/auth.service';
-import { Router } from '@angular/router'; // Importa Router correctamente
+import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
+import { FormBuilder, FormGroup, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
+
 
 @Component({
   selector: 'app-auth',
@@ -9,22 +11,25 @@ import Swal from 'sweetalert2';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent {
-  formMode: 'login' | 'register' = 'login'; // Estado inicial del formulario
+  formMode: 'login' | 'register' = 'login';
 
-  loginData = {
-    email: '',
-    password: ''
-  };
+  loginForm: FormGroup;
+  registerForm: FormGroup;
 
-  registerData = {
-    email: '',
-    password: '',
-    nombre: '',
-    apellidos: '',
-    telefono: ''
-  };
+  constructor(private authService: AuthService, private router: Router, private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
 
-  constructor(private authService: AuthService, private router: Router) {}
+    this.registerForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, this.passwordValidator]],
+      nombre: ['', Validators.required],
+      apellidos: ['', Validators.required],
+      telefono: ['', Validators.required]
+    });
+  }
 
   switchToLogin() {
     this.formMode = 'login';
@@ -35,64 +40,70 @@ export class AuthComponent {
   }
 
   onSubmitLogin() {
-    this.authService.login(this.loginData).subscribe({
-      next: (response: any) => {
-        localStorage.setItem('token', response.token);
-        this.router.navigateByUrl('/menu_principal'); // Redirige a la ruta principal o a donde necesites
-        this.authService.updateAuthStatus(true);
-
-        this.clearData();
-      },
-      error: (error: any) => {
-        Swal.fire({
-          icon: "error",
-          title: "Se ha producido un error al iniciar sesión",
-          text: error.error.message,
-        });
-
-        this.clearData();
-      }
-    });
+    if (this.loginForm.valid) {
+      this.authService.login(this.loginForm.value).subscribe({
+        next: (response: any) => {
+          localStorage.setItem('token', response.token);
+          this.router.navigateByUrl('/menu_principal');
+          this.authService.updateAuthStatus(true);
+          this.clearData();
+        },
+        error: (error: any) => {
+          Swal.fire({
+            icon: "error",
+            title: "Se ha producido un error al iniciar sesión",
+            text: error.error.message,
+          });
+          this.clearData();
+        }
+      });
+    }
   }
 
   onSubmitRegister() {
-    this.authService.register(this.registerData).subscribe({
-      next: (response: any) => {
-         this.switchToLogin();
-
-        Swal.fire({
-          title: "¡Su cuenta ha sido creada exitosamente!",
-          text: "Confirme su cuenta a través de mensaje en su email y luego inicie sesión para acceder a su cuenta",
-          icon: "success"
-        });
-        
-        this.clearData();
-      },
-      error: (error: any) => {
-        Swal.fire({
-          icon: "error",
-          title: "Se ha producido un error al registrarse",
-          text: error.error.message,
-        });
-
-        this.clearData();
-      }
-    });
+    if (this.registerForm.valid) {
+      this.authService.register(this.registerForm.value).subscribe({
+        next: (response: any) => {
+          this.switchToLogin();
+          Swal.fire({
+            title: "¡Su cuenta ha sido creada exitosamente!",
+            text: "Confirme su cuenta a través de mensaje en su email y luego inicie sesión para acceder a su cuenta",
+            icon: "success"
+          });
+          this.clearData();
+        },
+        error: (error: any) => {
+          Swal.fire({
+            icon: "error",
+            title: "Se ha producido un error al registrarse",
+            text: error.error.message,
+          });
+          this.clearData();
+        }
+      });
+    }
   }
 
-  // Limpiar datos de los formularios
   clearData() {
-    this.loginData = {
-      email: '',
-      password: ''
-    };
+    this.loginForm.reset();
+    this.registerForm.reset();
+  }
 
-    this.registerData = {
-      email: '',
-      password: '',
-      nombre: '',
-      apellidos: '',
-      telefono: ''
-    };
+  passwordValidator(control: AbstractControl): ValidationErrors | null {
+    const value = control.value;
+
+    if (!value) {
+      return null;
+    }
+
+    const hasMinLength = value.length >= 6;
+    const hasUpperCase = /[A-Z]/.test(value);
+    const hasLowerCase = /[a-z]/.test(value);
+    const hasNumeric = /[0-9]/.test(value);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(value);
+
+    const passwordValid = hasMinLength && hasUpperCase && hasLowerCase && hasNumeric && hasSpecialChar;
+
+    return !passwordValid ? { passwordStrength: 'La contraseña debe tener al menos 6 caracteres, una letra mayúscula, una letra minúscula, un número y un carácter especial.' } : null;
   }
 }
