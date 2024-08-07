b10c46826c449686a38709a6a2a16741d7c43b15
Dmytro Reva <dmytroreva2001@gmail.com>
2024-08-07 14:58:56 +0200

    Aplicar funcionalidades al html y definir metodos de gestión

--- src/app/gestion/gestion.component.ts
+++ src/app/gestion/gestion.component.ts
@@ -9,10 +9,9 @@ import { TipoProductoService } from '../services/tipo-producto.service';
 @Component({
   selector: 'app-gestion',
   templateUrl: './gestion.component.html',
-  styleUrl: './gestion.component.css'
+  styleUrl: './gestion.component.css',
 })
-export class GestionComponent implements OnInit{
-
+export class GestionComponent implements OnInit {
   elementos: any[] = [];
   nombreElemento: any;
 
@@ -23,97 +22,94 @@ export class GestionComponent implements OnInit{
     private route: ActivatedRoute,
     private router: Router,
     private tipoProductoService: TipoProductoService
-  ) {
-    
-  }
+  ) {}
 
   ngOnInit(): void {
-
     Swal.fire({
-      title: "Cargando...",
+      title: 'Cargando...',
       allowEscapeKey: true,
       allowOutsideClick: false,
       timerProgressBar: false,
       didOpen: () => {
         Swal.showLoading();
+      },
+    });
+
+    this.route.paramMap.subscribe((params) => {
+      this.nombreElemento = params.get('elemento');
+      if (!this.nombreElemento) {
+        this.router.navigate(['/menu_principal']);
+        return;
       }
     });
 
-      this.route.paramMap.subscribe(params => {
-        this.nombreElemento = params.get('elemento');
-        if (!this.nombreElemento) {
-          this.router.navigate(['/menu_principal']);
-          return;
-        }
+    // Verificar elementos a gestionar
+    if (this.nombreElemento === 'productos') {
+      // Obtener los productos del servicio al inicializar el componente
+      this.productoService.getProducts().subscribe((products) => {
+        this.elementos = products;
       });
-
-      // Verificar elementos a gestionar
-      if(this.nombreElemento === 'productos')
-      {
-        // Obtener los productos del servicio al inicializar el componente
-        this.productoService.getProducts().subscribe(products => {
-          this.elementos = products;
-        });
-      }
-      else if (this.nombreElemento === 'tatuajes')
-      {
+    } else if (this.nombreElemento === 'tatuajes') {
       // Obtener los tatuajes del servicio al inicializar el componente
-      this.tatuajesService.getTatuajes().subscribe(tatuajes => {
+      this.tatuajesService.getTatuajes().subscribe((tatuajes) => {
         this.elementos = tatuajes;
       });
-      }
-      else if (this.nombreElemento === 'tipos_productos')
-      {
+    } else if (this.nombreElemento === 'tipos_productos') {
       // Obtener los tatuajes del servicio al inicializar el componente
-      this.tipoProductoService.getTiposProducts().subscribe(tipos => {
+      this.tipoProductoService.getTiposProducts().subscribe((tipos) => {
         this.elementos = tipos;
       });
-      }
-      else
-      {
+    } else {
       // Obtener la información del servicio al inicializar el componente
-      this.informacionService.getInformacion().subscribe(info => {
+      this.informacionService.getInformacion().subscribe((info) => {
         this.elementos = info;
       });
-      }
+    }
 
-      Swal.close();
+    Swal.close();
   }
 
-  gestionarElemento(id: any) {
-    if(this.nombreElemento === 'productos')
-      {
-        // Mandar a ventana de nuevo producto con id en parametros
-        
-      }
-      else if (this.nombreElemento === 'tatuajes')
-      {
+  editarElemento(id: any) {
+    if (this.nombreElemento === 'productos') {
+      // Mandar a ventana de nuevo producto con id en parametros
+    } else if (this.nombreElemento === 'tatuajes') {
       // Mandar a ventana de nuevo tatuaje con id en parametros
-      this.tatuajesService.getTatuajes().subscribe(tatuajes => {
-        this.elementos = tatuajes;
-      });
-      }
-      else
-      {
+    } else if (this.nombreElemento === 'tipoProducto') {
+      // Mandar a ventana de nuevo tipoProducto con id en parametros
+    } else {
       // Mandar a ventana de nueva información con id en parametros
-      this.informacionService.getInformacion().subscribe(info => {
-        this.elementos = info;
-      });
-      }
+    }
   }
 
-formatString(input: string, upperCaseFirstLetter: boolean): string {
+  eliminarElemento(id: any) {
+    if (this.nombreElemento === 'productos') {
+      // Eliminar con su servicio correspondiente a elemento por su id
+    } else if (this.nombreElemento === 'tatuajes') {
+      // Eliminar con su servicio correspondiente a elemento por su id
+    } else if (this.nombreElemento === 'tipoProducto') {
+      // Eliminar con su servicio correspondiente a elemento por su id
+    } else {
+      // Eliminar con su servicio correspondiente a elemento por su id
+    }
+  }
+
+  formatString(input: string, upperCaseFirstLetter: boolean): string {
     let formattedString = input.replace(/[^a-zA-Z0-9áéíóúüñÁÉÍÓÚÜÑ]/g, ' ');
 
     if (upperCaseFirstLetter) {
-        formattedString = formattedString.replace(/([a-z])([A-Z])/g, '$1 $2').toLowerCase();
-        formattedString = formattedString.charAt(0).toUpperCase() + formattedString.slice(1);
+      formattedString = formattedString
+        .replace(/([a-z])([A-Z])/g, '$1 $2')
+        .toLowerCase();
+      formattedString =
+        formattedString.charAt(0).toUpperCase() + formattedString.slice(1);
     } else {
-        formattedString = formattedString.replace(/([a-z])([A-Z])/g, '$1 $2').toLowerCase();
-        formattedString = formattedString.charAt(0).toLowerCase() + formattedString.slice(1);
+      formattedString = formattedString
+        .replace(/([a-z])([A-Z])/g, '$1 $2')
+        .toLowerCase();
+      formattedString =
+        formattedString.charAt(0).toLowerCase() + formattedString.slice(1);
     }
 
     return formattedString;
-}
-
+  }
 }
